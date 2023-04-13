fbe7ece86d755b05fa940300f29de251a4b5dcba
yanis <yanis.bouger@gmail.com>
2023-04-13 22:00:29 +0200

    setup orm and swagger

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoursesController } from './courses/courses.controller';
+import { CoursesService } from './courses/courses.service';
+import { CommentsController } from './comments/comments.controller';
+import { CommentsService } from './comments/comments.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+    }),
+  ],
+  controllers: [AppController, CoursesController, CommentsController],
+  providers: [AppService, CoursesService, CommentsService],
 })
 export class AppModule {}
