e6cffa4a89f93efb963bf546d6bb2490e19d69ae
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2023-04-15 20:53:47 +0200

    enable swagger only in dev

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Erasmus+ API')
-    .setDescription('The API for Erasmus+ students at PUT')
-    .setVersion('1.0')
-    .build();
+  if (process.env.NODE_ENV !== 'production') {
+    const { DocumentBuilder, SwaggerModule } = await import('@nestjs/swagger');
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const config = new DocumentBuilder()
+      .setTitle('Erasmus+ API')
+      .setDescription('The API for Erasmus+ students at PUT')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(3000);
 }
